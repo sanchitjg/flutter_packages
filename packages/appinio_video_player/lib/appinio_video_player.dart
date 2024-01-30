3cdf3ad2aff97fad67690b7b1c0157a48f794b41
sanchitjg <sanchit.rawat@jungleegames.com>
2024-01-30 17:28:33 +0530

    un hid ProtectedCustomVideoPlayerController in appinio_video_player exports

--- packages/appinio_video_player/lib/appinio_video_player.dart
+++ packages/appinio_video_player/lib/appinio_video_player.dart
@@ -5,8 +5,7 @@ export 'src/custom_video_player.dart';
 export 'src/web_video_player/conditional_native_web_video_player/conditional_native_web_video_player.dart';
 export 'src/models/custom_video_player_progress_bar_settings.dart';
 export 'src/models/custom_video_player_settings.dart';
-export 'src/custom_video_player_controller.dart'
-    hide ProtectedCustomVideoPlayerController;
+export 'src/custom_video_player_controller.dart';
 
 // web exports
 export 'src/web_video_player/custom_video_player_web.dart';
